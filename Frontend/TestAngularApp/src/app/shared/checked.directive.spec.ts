6627e73c4d657165e74a05d1940ee01eeccd2cfb
matm94 <m.macias94@o2.pl>
2020-09-03 11:54:56 +0200

    Backup-v2

--- Frontend/TestAngularApp/src/app/shared/checked.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CheckedDirective } from './checked.directive';
-
-describe('CheckedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CheckedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- Frontend/TestAngularApp/src/app/shared/transform-task.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TransformTaskPipe } from './transform-task.pipe';
-
-describe('TransformTaskPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TransformTaskPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
